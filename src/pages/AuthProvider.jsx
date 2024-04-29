5d24f75b4449f9376ebe11edc510bb3dfa3707fc
Faisal778 <62201274+Faisal778@users.noreply.github.com>
2024-04-30 00:06:59 +0600

    created context

--- src/pages/AuthProvider.jsx
+++ src/pages/AuthProvider.jsx
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { createContext } from 'react';
+
+export const AuthContext = createContext(null);
 
 const AuthProvider = ({children}) => {
+
     return (
-        <div>
+        <AuthContext.Provider value = {}>
             {children}
-        </div>
+        </AuthContext.Provider>
     );
 };
 
